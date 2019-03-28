a2f9dbcd4178f0da3d48ceb0f28636d22462da96
18645956947 <18645956947@163.com>
2019-03-28 19:43:02 +0800

    开发用户模块

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -1,6 +1,12 @@
 package com.mmall.controller.portal;
 
 
+import com.mmall.common.Const;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+import com.mmall.service.IUserService;
+import org.omg.CORBA.PUBLIC_MEMBER;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +18,9 @@ import javax.servlet.http.HttpSession;
 @RequestMapping("/user/")
 public class UserController {
 
+    @Autowired
+    private IUserService iUserService;
+
     /**
      * 用户登录
      * @param username
@@ -19,8 +28,34 @@ public class UserController {
      * @return
      */
     @RequestMapping(value = "login.do", method = RequestMethod.POST)
-    @ResponseBody
-    public Object login(String username, String password, HttpSession session){
-        return null;
+    @ResponseBody//自定序列化为JSON
+    public ServerResponse<User> login(String username, String password, HttpSession session){
+        ServerResponse<User> response = iUserService.login(username, password);
+        if(response.isSuccess()){
+            session.setAttribute(Const.CURRENT_USER, response.getData());
+        }
+        return response;
     }
+
+    @RequestMapping(value = "logout.do", method = RequestMethod.GET)
+    @ResponseBody//自定序列化为JSON
+    public ServerResponse<String> logout(HttpSession session){
+        session.removeAttribute(Const.CURRENT_USER);
+        return ServerResponse.createBySuccess();
+    }
+
+    @RequestMapping(value = "register.do", method = RequestMethod.GET)
+    @ResponseBody//自定序列化为JSON
+    public ServerResponse<String> register(User user){
+        return iUserService.register(user);
+    }
+
+    @RequestMapping(value = "checkValid.do", method = RequestMethod.GET)
+    @ResponseBody//自定序列化为JSON
+    //校验用户名
+    public ServerResponse<String> checkValid(String str, String type){
+        return iUserService.checkValid(str, type);
+    }
+
+
 }
---
+++ src/main/java/com/mmall/service/impl/IUserServiceImpl.java
@@ -0,0 +1,79 @@
+package com.mmall.service.impl;
+
+import com.mmall.common.Const;
+import com.mmall.common.ServerResponse;
+import com.mmall.dao.UserMapper;
+import com.mmall.pojo.User;
+import com.mmall.service.IUserService;
+import com.mmall.util.MD5Util;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import sun.nio.cs.US_ASCII;
+
+import java.nio.channels.SeekableByteChannel;
+
+@Service("iUserService")
+public class IUserServiceImpl implements IUserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public ServerResponse<User> login(String username, String password) {
+        int resultCount = userMapper.checkUsername(username);
+        if(resultCount == 0){
+            return ServerResponse.createByErrorMessage("用户名不存在");
+        }
+
+        //todo 密码MD5登录
+        String md5Password = MD5Util.MD5EncodeUtf8(password);
+        User user = userMapper.selectLogin(username, md5Password);
+        if(user == null){
+            return ServerResponse.createByErrorMessage("密码错误");
+        }
+
+        user.setPassword(StringUtils.EMPTY);
+        return ServerResponse.createBySuccessMessage("登陆成功");
+    }
+
+    public ServerResponse<String> register(User user){
+        ServerResponse validResponse = this.checkValid(user.getUsername(), Const.USERNAME);
+        if(!validResponse.isSuccess()){
+            return validResponse;
+        }
+        validResponse = this.checkValid(user.getEmail(), Const.EMAIL);
+        if(!validResponse.isSuccess()){
+            return validResponse;
+        }
+        user.setRole(Const.Role.ROLE_CUSTOMER);
+        //MD5加密
+        user.setPassword(MD5Util.MD5EncodeUtf8(user.getPassword()));
+        int resultCount = userMapper.insert(user);
+        if(resultCount == 0){
+            return ServerResponse.createByErrorMessage("注册失败");
+        }
+        return ServerResponse.createBySuccessMessage("注册成功");
+    }
+
+    public ServerResponse<String> checkValid(String str, String type){
+        if( StringUtils.isNoneBlank(type)){
+            //开始校验
+           if(Const.USERNAME.equals(type)){
+               int resultCount = userMapper.checkUsername(str);
+               if(resultCount > 0){
+                   return ServerResponse.createByErrorMessage("用户名已存在");
+               }
+           }
+            if(Const.EMAIL.equals(type)){
+                int resultCount = userMapper.checkEmail(str);
+                if(resultCount > 0){
+                    return ServerResponse.createByErrorMessage("email已存在");
+                }
+            }
+        }else {
+            return ServerResponse.createByErrorMessage("参数错误");
+        }
+        return ServerResponse.createBySuccessMessage("校验成功");
+    }
+}
